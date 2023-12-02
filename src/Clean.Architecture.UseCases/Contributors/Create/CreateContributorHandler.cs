f96605a4df8028667ea3305534fb59460f711224
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2023-12-01 22:01:02 -0500

    Add CreateContributorCommand related code for AutoFixture  blog

--- src/Clean.Architecture.UseCases/Contributors/Create/CreateContributorHandler.cs
+++ src/Clean.Architecture.UseCases/Contributors/Create/CreateContributorHandler.cs
@@ -10,7 +10,7 @@ public class CreateContributorHandler(IRepository<Contributor> _repository)
   public async Task<Result<int>> Handle(CreateContributorCommand request,
     CancellationToken cancellationToken)
   {
-    var newContributor = new Contributor(request.Name);
+    var newContributor = new Contributor($"{request.FirstName} {request.LastName}");
     var createdItem = await _repository.AddAsync(newContributor, cancellationToken);
 
     return createdItem.Id;
--- tests/Clean.Architecture.UnitTests/UseCases/Contributors/CreateContributorHandlerHandle.cs
+++ tests/Clean.Architecture.UnitTests/UseCases/Contributors/CreateContributorHandlerHandle.cs
@@ -1,4 +1,8 @@
-﻿using Ardalis.SharedKernel;
+﻿using System.Reflection;
+using Ardalis.SharedKernel;
+using AutoFixture;
+using AutoFixture.Kernel;
+using AutoFixture.Xunit2;
 using Clean.Architecture.Core.ContributorAggregate;
 using Clean.Architecture.UseCases.Contributors.Create;
 using FluentAssertions;
@@ -7,29 +11,115 @@ using Xunit;
 
 namespace Clean.Architecture.UnitTests.UseCases.Contributors;
 
+
+// This file contains the unit tests for the CreateContributorHandler class
+// as well as the AutoFixture related classes described in this blog article.
+// https://blog.nimblepros.com/blogs/improving-your-tests-with-autofixture/
+
 public class CreateContributorHandlerHandle
 {
-  private readonly string _testName = "test name";
   private readonly IRepository<Contributor> _repository = Substitute.For<IRepository<Contributor>>();
-  private CreateContributorHandler _handler;
+  private readonly CreateContributorHandler _handler;
 
   public CreateContributorHandlerHandle()
   {
-      _handler = new CreateContributorHandler(_repository);
-  }
+    _handler = new CreateContributorHandler(_repository);
 
-  private Contributor CreateContributor()
-  {
-    return new Contributor(_testName);
+    _repository.AddAsync(Arg.Any<Contributor>(), Arg.Any<CancellationToken>())
+      .Returns(Task.FromResult(new Contributor("John Doe")));
   }
 
   [Fact]
-  public async Task ReturnsSuccessGivenValidName()
+  public async void ShouldCreateContributorGivenValidValuesFromVariables()
   {
-    _repository.AddAsync(Arg.Any<Contributor>(), Arg.Any<CancellationToken>())
-      .Returns(Task.FromResult(CreateContributor()));
-    var result = await _handler.Handle(new CreateContributorCommand(_testName), CancellationToken.None);
+    string email = "JohnDoe@Microsoft.com";
+    string firstName = "John";
+    string lastName = "Doe";
+    int followers = 45;
+    int following = 20;
+    int stars = 182;
+    string status = ContributorStatus.NotSet.Name;
+    var command = new CreateContributorCommand(
+      Email: email,
+      FirstName: firstName,
+      LastName: lastName,
+      Followers: followers,
+      Following: following,
+      Stars: stars,
+      Status: status);
 
-    result.IsSuccess.Should().BeTrue();    
+    var result = await _handler.Handle(command, CancellationToken.None);
+
+    result.IsSuccess.Should().BeTrue();
+  }
+
+  [Fact]
+  public async void ShouldCreateContributorGivenValuesFromFixture()
+  {
+    var fixture = new Fixture();
+    var command = fixture.Create<CreateContributorCommand>();
+
+    var result = await _handler.Handle(command, CancellationToken.None);
+
+    result.IsSuccess.Should().BeTrue();
+  }
+
+  [Fact]
+  public async void ShouldCreateContributorGivenValidValuesFromFixtureWithEmailExpression()
+  {
+    var fixture = new Fixture();
+    var command = fixture.Build<CreateContributorCommand>()
+      .With(c => c.Email, "JohnDoe@Microsoft.com")
+      .Create();
+
+    var result = await _handler.Handle(command, CancellationToken.None);
+
+    result.IsSuccess.Should().BeTrue();
+  }
+
+  [Theory, YourDomainData]
+  public async void ShouldCreateContributorGivenValidValuesFromAttributes(CreateContributorCommand command)
+  {
+    var result = await _handler.Handle(command, CancellationToken.None);
+
+    result.IsSuccess.Should().BeTrue();
+    result.Value.Should().BeGreaterThan(0);
+  }
+}
+
+public class ContributorStatusNotSetGenerator : ISpecimenBuilder
+{
+  public object Create(object request, ISpecimenContext context)
+  {
+    var props = request as PropertyInfo;
+
+    if (props is null)
+    {
+      return new NoSpecimen();
+    }
+
+    if (props.PropertyType != typeof(string) || props.Name != "Status")
+    {
+      return new NoSpecimen();
+    }
+
+    return ContributorStatus.NotSet.Name;
+  }
+}
+
+public class YourDomainSpecificFixture : Fixture
+{
+  public YourDomainSpecificFixture()
+  {
+      Customizations.Add(new ContributorStatusNotSetGenerator());
+    // Other potential customizations for your domain like a ContributorEmailGenerator, etc.
+  }
+}
+
+public class YourDomainDataAttribute : AutoDataAttribute
+{
+  public YourDomainDataAttribute() 
+      : base(() => new YourDomainSpecificFixture())
+  {
   }
 }
